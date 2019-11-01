42b28de1e4ba9695d4ec6fbcd418d282148381e8
Dinaerte Neto <>
2019-11-01 15:04:00 -0300

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 import App from "./App";
 
@@ -11,6 +12,7 @@ import 'semantic-ui-css/semantic.min.css';
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor} />
     <App />
   </Provider>,
   rootElement
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,17 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist"; 
+import storage from "redux-persist/lib/storage";    
+
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+const persistConfig = {
+    key: 'mol-todolist',
+    storage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
+
